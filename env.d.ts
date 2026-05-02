c2335f834549a41788e847ab20fedb129e0bc77c
Yahyazawadi <yahyazawadi278@gmail.com>
2026-05-02 15:17:54 +0300

    Add admin and R2 types to CloudflareEnv

--- env.d.ts
+++ env.d.ts
@@ -4,5 +4,10 @@ declare global {
   interface CloudflareEnv {
     DB: D1Database;
     BUCKET: R2Bucket;
+    ADMIN_USERNAME?: string;
+    ADMIN_PASSWORD?: string;
+    R2_ENDPOINT?: string;
+    R2_BUCKET_NAME?: string;
+    NEXT_PUBLIC_R2_PUBLIC_URL?: string;
   }
 }

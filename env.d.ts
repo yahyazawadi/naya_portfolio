559668b0f9871ab113e971acb6775d89ebc3adcb
Yahyazawadi <yahyazawadi278@gmail.com>
2026-05-02 14:54:58 +0300

    Update types for R2 bucket binding

--- env.d.ts
+++ env.d.ts
@@ -1,7 +1,8 @@
-import { D1Database } from '@cloudflare/workers-types';
+import { D1Database, R2Bucket } from '@cloudflare/workers-types';
 
 declare global {
   interface CloudflareEnv {
     DB: D1Database;
+    BUCKET: R2Bucket;
   }
 }
